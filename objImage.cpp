eb809d407e86cf8b5cd5fe922cfbae250f592a28
JuanGdev <j.aguilerahuerta@ugto.mx>
2021-11-22 12:35:50 -0600

    Nuevas especificaciones

--- objImage.cpp
+++ objImage.cpp
@@ -1,2 +1,14 @@
 #include "objImage.hpp"
 
+//Constructor por defecto
+PIXEL::PIXEL(): m_R(0), m_G(0), m_B(0), m_A(0){}
+
+PIXEL::PIXEL(unsigned char R, unsigned char G, unsigned char B, unsigned char A): PIXEL()
+{
+    if((R < 0) || (G < 0) || (B < 0) || (A < 0)) return;
+    
+    m_R = R;
+    m_G = G;
+    m_B = B;
+    m_A = A;
+}
--- objImage.hpp
+++ objImage.hpp
@@ -1,7 +1,24 @@
 #ifndef OBJIMAGE_HPP
 #define OBJIMAGE_HPP
 
+class Image{
 
+};
+
+class PIXEL
+{
+    public:
+        PIXEL::PIXEL(); //Constructor por defecto
+        PIXEL::PIXEL(unsigned char R, unsigned char G, unsigned char B, unsigned char A); //Constructor paramétrico
+
+    private:
+
+        unsigned char m_R; //Rojo
+        unsigned char m_G; //Verde
+        unsigned char m_B; //Azul
+        unsigned char m_A; //Transparencia
+    
+};
 
 
 #endif
